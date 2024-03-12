65c0c8b0eacda947b156dd354a662534eae59b8a
Rabin Shah <108661077+rabin9087@users.noreply.github.com>
2024-03-13 01:02:35 +1100

    configDB file change

--- src/configDb/configDb.js
+++ src/configDb/configDb.js
@@ -1,25 +1,9 @@
 // import { MongoClient } from 'mongodb'
 import mongoose from 'mongoose'
-const dbName = 'cricket_gear'
-// const client = new MongoClient(process.env.MONGO_URL)
-// export const connectMongoClient = async (documents) => {
-//     try {
-//         await client.connect()
-//         const table = client.db(dbName);
-//         const collection = table.collection(documents)
-//         return collection
-//     } catch (error) {
-//         console.log('error')
-//         return error
-//     }
-//     // finally {
-//     //     return client.close()
-//     // }
-// }
 
 export const connectToDatabase = () => {
     try {
-        const conn = mongoose.connect(process.env.MONGO_URL + '/' + dbName)
+        const conn = mongoose.connect(process.env.MONGO_URL)
         conn && console.log('connected to database')
     } catch (error) {
         console.log(error)
